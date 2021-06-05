1181ff4d2edce381e3b26a3ceb31c5f9381cbf83
tehloo <tehloo@gmail.com>
2021-06-06 02:04:46 +0900

    Unable to connect to server
    may need to apply certificate

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,9 +3,47 @@
 
 #include <iostream>
 #include "NetworkTCP.h"
+/* 
+* openssl 설치 및 설정 변경
+* - openssl 패키지 설치 - 
+* - include directory 추가 (프로젝트 > ... 속성 > C/C++ > 일반 > 추가 디렉토리) - C:\Program Files\OpenSSL-Win64\include
+* - library directory 추가 (프로젝트 > ... 속성 > 링커 > 일반 > 추가 디렉토리) - C:\Program Files\OpenSSL-Win64\lib
+* - library dependancy 추가 (프로젝트 > ... 속성 > 링커 > 입력 > 추가 종속성) - libcrypto.lib;libssl.lib
+* https://stackoverflow.com/questions/11383942/how-to-use-openssl-with-visual-studio
+*/ 
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+/*---------------------------------------------------------------------*/
+/*--- InitCTX - initialize the SSL engine.                          ---*/
+/*---------------------------------------------------------------------*/
+SSL_CTX* InitCTX(void)
+{
+    const SSL_METHOD* method;
+    SSL_CTX* ctx;
+
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();        /* Load cryptos, et.al. */
+    SSL_load_error_strings();            /* Bring in and register error messages */
+    method = SSLv23_client_method();        /* Create new client-method instance */
+    ctx = SSL_CTX_new(method);            /* Create new context */
+    if (ctx == NULL)
+    {
+        printf("ctx Error\n");
+    }
+
+    /* Cannot fail ??? */
+    const long flags = SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_COMPRESSION;
+    SSL_CTX_set_options(ctx, flags);
+    return ctx;
+}
+
 
 int main()
 {
+    SSL_CTX* ctx;
+    SSL* ssl;
+
     std::cout << "Hello World!\n";
 
     TTcpConnectedPort* TcpConnectedPort = NULL;
@@ -13,13 +51,23 @@ int main()
     const char* hostname = "172.27.59.14";
     const char* port = "5555";
 
+    ctx = InitCTX();
+    ssl = SSL_new(ctx);
+
     if ((TcpConnectedPort = OpenTcpConnection(hostname, port)) == NULL)  // Open UDP Network port
     {
         printf("OpenTcpConnection\n");
         return(-1);
     }
 
+    SSL_set_fd(ssl, TcpConnectedPort->ConnectedFd);
+    if (SSL_connect(ssl) == -1) {
+        printf("Connect failed\n");
+        return(-1);
+    }
+
     printf("Connected~!\n");
+    if (!ssl)
     {
         unsigned int imagesize;
         unsigned char* buff;	/* receive buffer */
@@ -40,8 +88,29 @@ int main()
         printf("%s\n", buff);
         return false;
     }
+    else 
+    {
+        unsigned int imagesize;
+        unsigned char* buff;	/* receive buffer */
+
+        SSL_read(ssl, &imagesize, sizeof(imagesize));
+        printf("imagesize??? %d\n", imagesize);
+        if (imagesize < 0) return false;
+
+        buff = new (std::nothrow) unsigned char[imagesize];
+        if (buff == NULL) return false;
+        memset(buff, imagesize, 0x00);
+
+        SSL_read(ssl, buff, imagesize);
+        printf("%s\n", buff);
+        return false;
+    }
+
+    SSL_free(ssl);
 
     CloseTcpConnectedPort(&TcpConnectedPort); // Close network port;
+    SSL_CTX_free(ctx);
+
     return 0;
 }
 
